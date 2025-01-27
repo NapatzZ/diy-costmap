fa1ba61866c672d2ded13a740ece982d08bded61
NapatzZ <Napat559977@outlook.co.th>
2025-01-28 02:05:04 +0700

    fix: color in rgb mode

--- const.py
+++ const.py
@@ -1,19 +1,19 @@
 class Config:
-
     """
-    Constants for cell states in a 2D occupancy grid.
+    Constants for cell states in a 2D occupancy grid, simplified for visualization.
     """
     __ALL_CONFIGS = {
-        "MAX_WIDHT_PIXEL" : 100,
-        "MAX_HEIGHT_PIXEL" : 100,
-        "FREE" : 255,
-        "OBSTACLE" : 0,
-        "VISITED" : 160,
-        "START" : 200,
-        "GOAL" : 50,
+        
+        "MAX_WIDTH_PIXEL": 100,
+        "MAX_HEIGHT_PIXEL": 100,
+
+        "FREE": (255, 255, 255),   # white
+        "OBSTACLE": (0, 0, 0),     # black
+        "VISITED": (0, 0, 255),    # blue
+        "START": (255, 0, 0),      # red
+        "GOAL": (0, 255, 0),       # green
     }
 
     @classmethod
     def get(cls, key):
         return cls.__ALL_CONFIGS[key]
-
