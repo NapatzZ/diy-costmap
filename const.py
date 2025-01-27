f3665f0454d94c4506bfb7782ae9141cfaef829f
NapatzZ <Napat559977@outlook.co.th>
2025-01-28 00:33:44 +0700

    fix: config as a classmethod

--- const.py
+++ const.py
@@ -1,12 +1,19 @@
-"""
-Constants for cell states in a 2D occupancy grid.
-"""
+class Config:
 
-MAX_WIDHT_PIXEL = 100
-MAX_HEIGHT_PIXEL = 100 
-FREE = 255      # White
-OBSTACLE = 0    # Black
-UNKNOWN = 128   # Gray
-VISITED = 160   # Slightly off-white (for marking visited cells)
-START = 200     # Distinct grayscale for start
-GOAL = 50       # Distinct grayscale for goal
+    """
+    Constants for cell states in a 2D occupancy grid.
+    """
+    __ALL_CONFIGS = {
+        "MAX_WIDHT_PIXEL" : 100,
+        "MAX_HEIGHT_PIXEL" : 100,
+        "FREE" : 255,
+        "OBSTACLE" : 0,
+        "VISITED" : 160,
+        "START" : 200,
+        "GOAL" : 50,
+    }
+
+    @classmethod
+    def get(cls, key):
+        return cls.__ALL_CONFIGS[key]
+
